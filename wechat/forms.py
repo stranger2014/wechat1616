42396d7464e6e3e6317abb72e9e781b521d095b0
stranger2014 <rangerofalgo@163.com>
2016-10-22 13:44:20 +0800

    Add files via upload

---
+++ wechat/forms.py
@@ -0,0 +1,9 @@
+#forms.py
+'''from  django.forms import ModelForm
+from wechat.models import Img
+
+class ImgForm(ModelForm):
+    class Meta:
+        model=Img'''
+import  django.forms
+
---
+++ wechat/handlers.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+#
+from wechat.wrapper import WeChatHandler
+
+
+__author__ = "Epsirom"
+
+
+class ErrorHandler(WeChatHandler):
+
+    def check(self):
+        return True
+
+    def handle(self):
+        return self.reply_text('对不起，服务器现在有点忙，暂时不能给您答复 T T')
+
+
+class DefaultHandler(WeChatHandler):
+
+    def check(self):
+        return True
+
+    def handle(self):
+        return self.reply_text('对不起，没有找到您需要的信息:(')
+
+
+class HelpOrSubscribeHandler(WeChatHandler):
+
+    def check(self):
+        return self.is_text('帮助', 'help') or self.is_event('scan', 'subscribe') or \
+               self.is_event_click(self.view.event_keys['help'])
+
+    def handle(self):
+        return self.reply_single_news({
+            'Title': self.get_message('help_title'),
+            'Description': self.get_message('help_description'),
+            'Url': self.url_help(),
+        })
+
+
+class UnbindOrUnsubscribeHandler(WeChatHandler):
+
+    def check(self):
+        return self.is_text('解绑') or self.is_event('unsubscribe')
+
+    def handle(self):
+        self.user.student_id = ''
+        self.user.save()
+        return self.reply_text(self.get_message('unbind_account'))
+
+
+class BindAccountHandler(WeChatHandler):
+
+    def check(self):
+        return self.is_text('绑定') or self.is_event_click(self.view.event_keys['account_bind'])
+
+    def handle(self):
+        return self.reply_text(self.get_message('bind_account'))
+
+
+class BookEmptyHandler(WeChatHandler):
+
+    def check(self):
+        return self.is_event_click(self.view.event_keys['book_empty'])
+
+    def handle(self):
+        return self.reply_text(self.get_message('book_empty'))
+
+class calHandle(WeChatHandler):
+
+    def check(self):
+        content = self.input['Content']
+        str = '0123456789+-*/()'
+        for ch in content:
+            if ch in str:
+                continue
+            else:
+                return False
+        return True
+
+    def handle(self):
+        try:
+            result = eval(self.input['Content'])
+        except:
+            return self.reply_text("输入有误!请重新输入..")
+        return self.reply_text(result);
